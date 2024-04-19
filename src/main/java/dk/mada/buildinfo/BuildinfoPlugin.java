c2f414acb69f736c666f6c8297b62ef1868094a5
Jesper Skov <jskov@mada.dk>
2024-04-19 15:18:05 +0200

    indent

--- src/main/java/dk/mada/buildinfo/BuildinfoPlugin.java
+++ src/main/java/dk/mada/buildinfo/BuildinfoPlugin.java
@@ -4,7 +4,6 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.logging.Logger;
 
-
 public final class BuildinfoPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
