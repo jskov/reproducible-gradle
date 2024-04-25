33b186dee77b463419686933f222b11608b7e8f3
Jesper Skov <jskov@mada.dk>
2024-04-25 07:37:48 +0200

    Cut out task and tests, make archives in all projects reproducible

--- src/main/java/dk/mada/buildinfo/BuildinfoPlugin.java
+++ src/main/java/dk/mada/buildinfo/BuildinfoPlugin.java
@@ -2,14 +2,11 @@ package dk.mada.buildinfo;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.logging.Logger;
 import org.gradle.api.tasks.bundling.AbstractArchiveTask;
 
-import dk.mada.buildinfo.tasks.GenerateBuildInfo;
-
 /**
- * Plugin providing a generateBuildInfo task.
- *
- * The plugin also configures archive tasks to be reproducible.
+ * Plugin configures archive tasks to be reproducible.
  */
 public final class BuildinfoPlugin implements Plugin<Project> {
 
@@ -22,12 +19,22 @@ public final class BuildinfoPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
-        project.getTasks().register("generateBuildInfo", GenerateBuildInfo.class)
-                .configure(GenerateBuildInfo::lazyConfiguration);
+        Logger logger = project.getLogger();
+        
+        if (project.getParent() != null) {
+            logger.warn("Plugin should only be applied on the main project");
+            return;
+        }
 
-        project.getTasks().withType(AbstractArchiveTask.class).configureEach(jar -> {
-            jar.setReproducibleFileOrder(true);
-            jar.setPreserveFileTimestamps(false);
+        project.allprojects(aProject -> {
+            aProject.afterEvaluate(postEvaluatedProject -> {
+                logger.info("Configure jars in evaluated {}", postEvaluatedProject);
+                postEvaluatedProject.getTasks().withType(AbstractArchiveTask.class).configureEach(jar -> {
+                    logger.info("Making {} reproducible", jar.getName());
+                    jar.setReproducibleFileOrder(true);
+                    jar.setPreserveFileTimestamps(false);
+                });
+            });
         });
     }
 }
